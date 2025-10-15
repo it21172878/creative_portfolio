463c92db2f8fdf754e6c33cfdb56f527b83258b2
Dilanka <prasadliyanagamad@gmail.com>
2025-10-15 18:03:06 +0530

    Services Section

--- components/Home/Home.tsx
+++ components/Home/Home.tsx
@@ -1,7 +1,14 @@
 import React from "react";
+import Hero from "./Hero/Hero";
+import Services from "./Services/Services";
 
 const Home = () => {
-  return <div className=" overflow-hidden">Home</div>;
+  return (
+    <div className=" overflow-hidden">
+      <Hero />
+      <Services />
+    </div>
+  );
 };
 
 export default Home;
